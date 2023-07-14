32f8b14124584d1a0a96d67895ac0de8fd0a1d00
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2023-07-14 13:18:10 -0400

    Install Plotly

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ const dfd = require("danfojs-node");
 (async function() {
     df = await dfd.readJSON(__dirname + "/filteredGenderedData.json");
     df.head().print();
+    //df.plot("Born: Year ", "Rough Age (death year - birth year)", ["Predicted Gender"]).bar();
 })();
